bf9485fd426f7b30d3cd419afd17e4631f8046e9
ranjitm10 <ranjitmohite00@gmail.com>
2020-12-05 21:19:25 +0530

    last commit

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-var username = prompt("enter u r name")
-var welcomemessage = "this script works! " + username;
-alert(welcomemessage);
+var btn = document.querySelector("#btn");
+var txtIn = document.querySelector("#txtInput");
+var output = document.querySelector("#divOut");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translateURL(text){
+	return serverURL + "?" + "text=" + text; 
+}
+
+function errorHandler(error){
+	console.log("error occured "+error);
+	alert("Server is down! Please try after some time.");
+}
+
+btn.addEventListener("click",function btnHandler(){
+	fetch(translateURL(txtIn.value)).then(response => response.json()).then(json=>{
+		divOut.innerText=json.contents.translated;
+	}).catch(errorHandler);
+});
