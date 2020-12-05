1e2bb8095ce8251b2aca52aa3bf29cee801a6abb
ranjitm10 <ranjitmohite00@gmail.com>
2020-12-05 23:03:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,24 +14,25 @@ function errorHandler(error){
 }
 
 
-function btnHandler() {
-    var inputText = txtInput.value; // taking input
+// function btnHandler() {
+//     var inputText = txtInput.value; // taking input
 
-    // calling server for processing
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            divOut.innerText = translatedText; // output
-           })
-        .catch(errorHandler)
-};
+//     // calling server for processing
+//     fetch(getTranslationURL(inputText))
+//         .then(response => response.json())
+//         .then(json => {
+//             var translatedText = json.contents.translated;
+//             divOut.innerText = translatedText; // output
+//            })
+//         .catch(errorHandler)
+// };
 
-btnTranslate.addEventListener("click", btnHandler)
+// btnTranslate.addEventListener("click", btnHandler)
 
-
-// btn.addEventListener("click",function btnHandler(){
-// 	fetch(translateURL(txtIn.value)).then(response => response.json()).then(json=>{
-// 		divOut.innerText=json.contents.translated;
-// 	}).catch(errorHandler);
-// });
+function btnHandler(){
+btn.addEventListener("click",function btnHandler(){
+	fetch(translateURL(txtIn.value)).then(response => response.json()).then(json=>{
+		divOut.innerText=json.contents.translated;
+	}).catch(errorHandler);
+});
+}
